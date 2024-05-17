bd7e8d5f563d0f0cd62c99d7c5d14a4f17c7a0ac
Hyesung0426 <141066717+Hyesung0426@users.noreply.github.com>
2024-05-17 16:08:06 +0900

    샘플 코드

---
+++ Team2_Website/src/main/java/com/team2_website/camealone/Team2WebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.team2_website.camealone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2WebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team2WebsiteApplication.class, args);
+    }
+
+}
