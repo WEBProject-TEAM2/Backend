83318b9d008e223fe41d13ac4a4086b4d6e7edb8
LoadingV4 <nk76802@naver.com>
2024-05-17 17:12:36 +0900

    first commit

---
+++ Backend-main/README.md
@@ -0,0 +1,2 @@
+단톡방에 있는 ERD 참고해서 DB 우선 구축 후 src/main/resources/application.properties 파일 수정해서 DB 접근 활성화
+스프링부트 실행 시 login 창이 뜨는데 ID부분에는 user, PW부분에는 실행 시 콘솔창에 나오는 비밀번호 입력해서 접근하면 됨
---
+++ Backend-main/Team2_Website/src/test/java/com/team2_website/camealone/Team2WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2_website.camealone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
