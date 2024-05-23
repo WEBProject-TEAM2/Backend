43a85698e61a6d855eff871540a22d66eb66a111
Hyesung0426 <141066717+Hyesung0426@users.noreply.github.com>
2024-05-23 18:05:31 +0900

    Delete CameAlone directory

--- CameAlone/src/main/java/org/team2project/camealone/repository/PlacesRepository.java
+++
@@ -1,9 +0,0 @@
-package org.team2project.camealone.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.team2project.camealone.entity.Places;
-
-@Repository
-public interface PlacesRepository extends JpaRepository<Places, Long> {
-}
--- CameAlone/src/main/java/org/team2project/camealone/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.team2project.camealone.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.team2project.camealone.entity.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
